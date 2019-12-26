773e7b2526ae37f2c8aa193dca9dc42d043039c3
Udit Vasu <udit.vasu@getpostman.com>
2019-12-26 12:03:15 +0530

    Update unit tests

--- test/unit/encoder.test.js
+++ test/unit/encoder.test.js
@@ -1,116 +1,118 @@
-var expect = require('chai').expect,
-    urlEncoder = require('../../index.js'),
-    sdk = require('postman-collection');
+var sdk = require('postman-collection'),
+    expect = require('chai').expect,
+
+    encoder = require('../../encoder'),
+    urlEncoder = require('../../index');
 
 describe('encoder', function () {
-    describe('encodeAuth()', function () {
+    describe('encodeUserInfo()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encoder.encodeAuth({})).to.eql('');
-            expect(urlEncoder.encoder.encodeAuth(null)).to.eql('');
+            expect(encoder.encodeUserInfo({})).to.eql('');
+            expect(encoder.encodeUserInfo(null)).to.eql('');
         });
 
         it('should encode unicode auth param', function () {
-            expect(urlEncoder.encoder.encodeAuth('𝌆й你ス')).to.eql('%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9');
+            expect(encoder.encodeUserInfo('𝌆й你ス')).to.eql('%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9');
         });
 
         it('should not encode already encoded auth param', function () {
-            expect(urlEncoder.encoder.encodeAuth('%25')).to.eql('%25');
+            expect(encoder.encodeUserInfo('%25')).to.eql('%25');
         });
     });
 
     describe('encodeHost()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encoder.encodeHost({})).to.eql('');
-            expect(urlEncoder.encoder.encodeHost(null)).to.eql('');
+            expect(encoder.encodeHost({})).to.eql('');
+            expect(encoder.encodeHost(null)).to.eql('');
         });
 
         it('should accept host as an array', function () {
-            expect(urlEncoder.encoder.encodeHost(['www', 'postman', 'com'])).to.eql('www.postman.com');
+            expect(encoder.encodeHost(['www', 'postman', 'com'])).to.eql('www.postman.com');
         });
 
         it('should encode unicode host', function () {
-            expect(urlEncoder.encoder.encodeHost('邮差.com')).to.eql('xn--nstq34i.com');
+            expect(encoder.encodeHost('邮差.com')).to.eql('xn--nstq34i.com');
         });
 
         it('should not encode already encoded unicode host', function () {
-            expect(urlEncoder.encoder.encodeHost('xn--nstq34i.com')).to.eql('xn--nstq34i.com');
+            expect(encoder.encodeHost('xn--nstq34i.com')).to.eql('xn--nstq34i.com');
         });
     });
 
     describe('encodePath()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encoder.encodePath({})).to.eql('');
-            expect(urlEncoder.encoder.encodePath(null)).to.eql('');
+            expect(encoder.encodePath({})).to.eql('');
+            expect(encoder.encodePath(null)).to.eql('');
         });
 
         it('should accept path as an array', function () {
-            expect(urlEncoder.encoder.encodePath(['foo', 'bar', 'baz'])).to.eql('foo/bar/baz');
+            expect(encoder.encodePath(['foo', 'bar', 'baz'])).to.eql('foo/bar/baz');
         });
 
         it('should encode special characters in path', function () {
-            expect(urlEncoder.encoder.encodePath('/ /"/#/</>/?/`/{/}')).to.eql('/%20/%22/%23/%3C/%3E/%3F/%60/%7B/%7D');
+            expect(encoder.encodePath('/ /"/#/</>/?/`/{/}')).to.eql('/%20/%22/%23/%3C/%3E/%3F/%60/%7B/%7D');
         });
 
         it('should encode unicode characters in path', function () {
-            expect(urlEncoder.encoder.encodePath('/𝌆/й/你/ス')).to.eql('/%F0%9D%8C%86/%D0%B9/%E4%BD%A0/%E3%82%B9');
+            expect(encoder.encodePath('/𝌆/й/你/ス')).to.eql('/%F0%9D%8C%86/%D0%B9/%E4%BD%A0/%E3%82%B9');
         });
 
         it('should not encode already encoded characters in path', function () {
-            expect(urlEncoder.encoder.encodePath('/%25')).to.eql('/%25');
+            expect(encoder.encodePath('/%25')).to.eql('/%25');
         });
     });
 
     // @todo this API is no longer exposed
-    describe.skip('encodeQueryParam()', function () {
+    describe('encodeQueryParam()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encodeQueryParam(null)).to.eql('');
-            expect(urlEncoder.encodeQueryParam(undefined)).to.eql('');
+            expect(encoder.encodeQueryParam(null)).to.eql('');
+            expect(encoder.encodeQueryParam(undefined)).to.eql('');
         });
 
         it('should accept string argument', function () {
-            expect(urlEncoder.encodeQueryParam('邮差')).to.eql('%E9%82%AE%E5%B7%AE');
+            expect(encoder.encodeQueryParam('邮差')).to.eql('%E9%82%AE%E5%B7%AE');
         });
 
         it('should properly handle param without key', function () {
-            expect(urlEncoder.encodeQueryParam({ value: 'bar' })).to.eql('=bar');
+            expect(encoder.encodeQueryParam({ value: 'bar' })).to.eql('=bar');
         });
 
         it('should properly handle param without value', function () {
-            expect(urlEncoder.encodeQueryParam({ key: 'foo' })).to.eql('foo=');
+            expect(encoder.encodeQueryParam({ key: 'foo' })).to.eql('foo=');
         });
 
         it('should properly handle param without key and value', function () {
-            expect(urlEncoder.encodeQueryParam({})).to.eql('');
+            expect(encoder.encodeQueryParam({})).to.eql('');
         });
 
         it('should encode special characters in key', function () {
-            expect(urlEncoder.encodeQueryParam({ key: ' "#\'<>', value: 'bar' })).to.eql('%20%22%23%27%3C%3E=bar');
+            expect(encoder.encodeQueryParam({ key: ' "#\'<>', value: 'bar' })).to.eql('%20%22%23%27%3C%3E=bar');
         });
 
         it('should encode special characters in value', function () {
-            expect(urlEncoder.encodeQueryParam({ key: 'foo', value: ' "#\'<>' })).to.eql('foo=%20%22%23%27%3C%3E');
+            expect(encoder.encodeQueryParam({ key: 'foo', value: ' "#\'<>' })).to.eql('foo=%20%22%23%27%3C%3E');
         });
 
         it('should encode unicode characters in key', function () {
-            expect(urlEncoder.encodeQueryParam({ key: '𝌆й你ス', value: 'bar' }))
+            expect(encoder.encodeQueryParam({ key: '𝌆й你ス', value: 'bar' }))
                 .to.eql('%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9=bar');
         });
 
         it('should encode unicode characters in value', function () {
-            expect(urlEncoder.encodeQueryParam({ key: 'foo', value: '𝌆й你ス' }))
+            expect(encoder.encodeQueryParam({ key: 'foo', value: '𝌆й你ス' }))
                 .to.eql('foo=%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9');
         });
 
         it('should not encode already encoded characters', function () {
-            expect(urlEncoder.encodeQueryParam({ key: '%25', value: '%25' })).to.eql('%25=%25');
+            expect(encoder.encodeQueryParam({ key: '%25', value: '%25' })).to.eql('%25=%25');
         });
     });
 
     describe('encodeQueryParams()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encoder.encodeQueryParams(null)).to.eql('');
-            expect(urlEncoder.encoder.encodeQueryParams(undefined)).to.eql('');
-            expect(urlEncoder.encoder.encodeQueryParams('foo')).to.eql('');
+            expect(encoder.encodeQueryParams(null)).to.eql('');
+            expect(encoder.encodeQueryParams(undefined)).to.eql('');
+            expect(encoder.encodeQueryParams('foo')).to.eql('');
         });
 
         it('should accept array of param objects in the argument', function () {
@@ -119,7 +121,7 @@ describe('encoder', function () {
                 { key: 'q2', value: 'v2' }
             ];
 
-            expect(urlEncoder.encoder.encodeQueryParams(params)).to.eql('q1=v1&q2=v2');
+            expect(encoder.encodeQueryParams(params)).to.eql('q1=v1&q2=v2');
         });
 
         it('should accept params object in the argument', function () {
@@ -128,7 +130,7 @@ describe('encoder', function () {
                 q2: 'v2'
             };
 
-            expect(urlEncoder.encoder.encodeQueryParams(params)).to.eql('q1=v1&q2=v2');
+            expect(encoder.encodeQueryParams(params)).to.eql('q1=v1&q2=v2');
         });
 
         it('should properly handle multiple values for a key in params object', function () {
@@ -137,33 +139,24 @@ describe('encoder', function () {
                 q2: 'v2'
             };
 
-            expect(urlEncoder.encoder.encodeQueryParams(params)).to.eql('q1=v1_1&q1=v1_2&q2=v2');
+            expect(encoder.encodeQueryParams(params)).to.eql('q1=v1_1&q1=v1_2&q2=v2');
         });
 
-        it('should exclude disabled params when second argument is true', function () {
+        it('should exclude disabled params by default', function () {
             var params = [
                 { key: 'q1', value: 'v1', disabled: true },
                 { key: 'q2', value: 'v2' }
             ];
 
-            expect(urlEncoder.encoder.encodeQueryParams(params, true)).to.eql('q2=v2');
-        });
-
-        it('should not exclude disabled params when second argument is false', function () {
-            var params = [
-                { key: 'q1', value: 'v1', disabled: true },
-                { key: 'q2', value: 'v2' }
-            ];
-
-            expect(urlEncoder.encoder.encodeQueryParams(params, false)).to.eql('q1=v1&q2=v2');
+            expect(encoder.encodeQueryParams(params)).to.eql('q2=v2');
         });
     });
 
-    describe('stringifyUrlencodedBody()', function () {
+    describe('encodeQueryString()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.stringifyUrlencodedBody(null)).to.eql('');
-            expect(urlEncoder.stringifyUrlencodedBody(undefined)).to.eql('');
-            expect(urlEncoder.stringifyUrlencodedBody('foo')).to.eql('');
+            expect(urlEncoder.encodeQueryString(null)).to.eql('');
+            expect(urlEncoder.encodeQueryString(undefined)).to.eql('');
+            expect(urlEncoder.encodeQueryString('foo')).to.eql('');
         });
 
         it('should properly stringify given body', function () {
@@ -173,7 +166,7 @@ describe('encoder', function () {
                 },
                 stringifiedBody = 'foo=bar&alpha=beta&alpha=gama';
 
-            expect(urlEncoder.stringifyUrlencodedBody(urlencodedBody)).to.eql(stringifiedBody);
+            expect(urlEncoder.encodeQueryString(urlencodedBody)).to.eql(stringifiedBody);
         });
 
         it('should encode characters: `!\'()*`', function () {
@@ -182,27 +175,27 @@ describe('encoder', function () {
                 },
                 stringifiedBody = 'extraChars=%21%27%28%29%2A';
 
-            expect(urlEncoder.stringifyUrlencodedBody(urlencodedBody)).to.eql(stringifiedBody);
+            expect(urlEncoder.encodeQueryString(urlencodedBody)).to.eql(stringifiedBody);
         });
     });
 
-    describe('encodeHash()', function () {
+    describe('encodeFragment()', function () {
         it('should return empty string for invalid argument', function () {
-            expect(urlEncoder.encoder.encodeHash(null)).to.eql('');
-            expect(urlEncoder.encoder.encodeHash(undefined)).to.eql('');
-            expect(urlEncoder.encoder.encodeHash({})).to.eql('');
+            expect(encoder.encodeFragment(null)).to.eql('');
+            expect(encoder.encodeFragment(undefined)).to.eql('');
+            expect(encoder.encodeFragment({})).to.eql('');
         });
 
         it('should encode unicode characters in given hash', function () {
-            expect(urlEncoder.encoder.encodeHash('𝌆й你ス')).to.eql('%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9');
+            expect(encoder.encodeFragment('𝌆й你ス')).to.eql('%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9');
         });
 
         it('should encode special characters in given hash', function () {
-            expect(urlEncoder.encoder.encodeHash(' "<>`')).to.eql('%20%22%3C%3E%60');
+            expect(encoder.encodeFragment(' "<>`')).to.eql('%20%22%3C%3E%60');
         });
 
         it('should not encode already encoded characters', function () {
-            expect(urlEncoder.encoder.encodeHash('%25')).to.eql('%25');
+            expect(encoder.encodeFragment('%25')).to.eql('%25');
         });
     });
 
@@ -220,7 +213,7 @@ describe('encoder', function () {
                 query: null,
                 pathname: null,
                 path: null,
-                href: null
+                href: ''
             };
 
             expect(urlEncoder.toNodeUrl(null)).to.eql(emptyUrl);
--- test/unit/legacy-encoder.test.js
+++ test/unit/legacy-encoder.test.js
@@ -1,91 +1,92 @@
-var expect = require('chai').expect,
-    urlEncoder = require('../../index.js').legacyEncoder,
-    sdk = require('postman-collection');
+var sdk = require('postman-collection'),
+    expect = require('chai').expect,
+
+    urlEncoder = require('../../index').toLegacyNodeUrl;
 
 describe('Legacy encoder', function () {
     describe('Url Encoding fix of node\'s bug', function () {
         describe('Encode', function () {
             it('%', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?charwithPercent=%foo');
-                expect(encodedUrl).to.equal('http://foo.bar.com?charwithPercent=%25foo');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?charwithPercent=%foo').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?charwithPercent=%25foo');
             });
 
             it('( and )', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=foo(a)');
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=foo%28a%29');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=foo(a)').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=foo%28a%29');
             });
 
             it('Space', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=foo bar');
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=foo%20bar');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=foo bar').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=foo%20bar');
             });
 
             it('multibyte character "𝌆"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?multibyte=𝌆');
-                expect(encodedUrl).to.equal('http://foo.bar.com?multibyte=%F0%9D%8C%86');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?multibyte=𝌆').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?multibyte=%F0%9D%8C%86');
             });
 
             it('Russian charcters "обязательный"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=обязательный');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=обязательный').href;
 
                 // eslint-disable-next-line max-len
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9');
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9');
             });
 
             it('Chinese charcters "你好"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=你好');
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=%E4%BD%A0%E5%A5%BD');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=你好').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=%E4%BD%A0%E5%A5%BD');
             });
 
             it('Japanese charcters "ハローポストマン"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=ハローポストマン');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=ハローポストマン').href;
 
                 // eslint-disable-next-line max-len
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=%E3%83%8F%E3%83%AD%E3%83%BC%E3%83%9D%E3%82%B9%E3%83%88%E3%83%9E%E3%83%B3');
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=%E3%83%8F%E3%83%AD%E3%83%BC%E3%83%9D%E3%82%B9%E3%83%88%E3%83%9E%E3%83%B3');
             });
 
             it('Partial Russian charcters "Hello Почтальон"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=Hello Почтальон');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=Hello Почтальон').href;
 
                 expect(encodedUrl)
-                    .to.equal('http://foo.bar.com?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD');
+                    .to.equal('http://foo.bar.com/?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD');
             });
         });
 
         describe('Do not encode', function () {
 
             it('/, &, =, :, ?, +', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=b&c=d&e=f+g');
-                expect(encodedUrl).to.equal('http://foo.bar.com?a=b&c=d&e=f+g');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=b&c=d&e=f+g').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a=b&c=d&e=f+g');
             });
 
             it('[ and ]', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a[0]=foo&a[1]=bar');
-                expect(encodedUrl).to.equal('http://foo.bar.com?a[0]=foo&a[1]=bar');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a[0]=foo&a[1]=bar').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?a[0]=foo&a[1]=bar');
             });
 
             it('pre encoded text( must avoid double encoding) - "email=foo%2Bbar%40domain.com"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?email=foo%2Bbar%40domain.com');
-                expect(encodedUrl).to.equal('http://foo.bar.com?email=foo%2Bbar%40domain.com');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?email=foo%2Bbar%40domain.com').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?email=foo%2Bbar%40domain.com');
             });
 
             it('pre-encoded multibyte character - "multibyte=%F0%9D%8C%86"', function () {
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?multibyte=%F0%9D%8C%86');
-                expect(encodedUrl).to.equal('http://foo.bar.com?multibyte=%F0%9D%8C%86');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?multibyte=%F0%9D%8C%86').href;
+                expect(encodedUrl).to.equal('http://foo.bar.com/?multibyte=%F0%9D%8C%86');
             });
 
             // eslint-disable-next-line max-len
             it('pre-encoded russian text - "a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD"', function () {
                 // eslint-disable-next-line max-len
-                var encodedUrl = urlEncoder.encode('http://foo.bar.com?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD');
+                var encodedUrl = urlEncoder('http://foo.bar.com/?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD').href;
 
                 expect(encodedUrl)
-                    .to.equal('http://foo.bar.com?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD');
+                    .to.equal('http://foo.bar.com/?a=Hello%20%D0%9F%D0%BE%D1%87%D1%82%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD');
             });
         });
     });
 
-    describe('toNodeUrl()', function () {
+    describe('toLegacyNodeUrl()', function () {
         it('should convert postman URL to string properly', function () {
             var url = new sdk.Url({
                     auth: '',
@@ -113,7 +114,7 @@ describe('Legacy encoder', function () {
                     path: '/foo/bar?foo1=bar1&foo2=bar2',
                     href: 'http://www.getpostman.com:3000/foo/bar?foo1=bar1&foo2=bar2#postman'
                 },
-                nodeUrl = urlEncoder.toNodeUrl(url.toString());
+                nodeUrl = urlEncoder(url.toString());
 
             expect(nodeUrl).to.include(expectedResult);
         });
@@ -145,7 +146,7 @@ describe('Legacy encoder', function () {
                     path: '/foo/bar?foo1=%E5%B7%AE&foo2=bar2',
                     href: 'http://www.xn--wtqy8jqx5dmsazn.com:3000/foo/bar?foo1=%E5%B7%AE&foo2=bar2#postman'
                 },
-                nodeUrl = urlEncoder.toNodeUrl(url.toString());
+                nodeUrl = urlEncoder(url.toString());
 
             expect(nodeUrl).to.include(expectedResult);
         });
