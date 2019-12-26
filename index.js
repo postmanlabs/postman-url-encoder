c0dac5fdaf84bb2cee076ce38fdc1d778747ac73
Udit Vasu <udit.vasu@getpostman.com>
2019-12-26 12:02:42 +0530

    Update main modules API

--- index.js
+++ index.js
@@ -1,17 +1,227 @@
-/**!
- * @license Copyright 2019 Postdot Technologies, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
- */
-var PostmanUrlEncoder = require('./lib/index');
-PostmanUrlEncoder.legacyEncoder = require('./lib/legacy');
+const sdk = require('postman-collection'),
+    querystring = require('querystring'),
 
-module.exports = PostmanUrlEncoder;
+    legacy = require('./legacy'),
+    encoder = require('./encoder'),
+    QUERY_ENCODE_SET = require('./encoder/encode-set').QUERY_ENCODE_SET,
+
+    E = '',
+    COLON = ':',
+    STRING = 'string',
+    OBJECT = 'object',
+    FUNCTION = 'function',
+    DOUBLE_SLASH = '//',
+    DEFAULT_PROTOCOL = 'http',
+
+    QUERY_SEPARATOR = '?',
+    SEARCH_SEPARATOR = '#',
+    PROTOCOL_SEPARATOR = '://',
+    AUTH_CREDENTIALS_SEPARATOR = '@',
+
+    /**
+     * Protocols that always contain a // bit.
+     *
+     * @private
+     * @see {@link https://github.com/nodejs/node/blob/v10.17.0/lib/url.js#L91}
+     */
+    SLASHED_PROTOCOLS = new Set([
+        'file:',
+        'ftp:',
+        'gopher:',
+        'http:',
+        'https:',
+        'ws:',
+        'wss:'
+    ]);
+
+/**
+ * Percent-encode the given string using QUERY_ENCODE_SET.
+ *
+ * @example
+ * // returns 'foo%20%22%23%26%27%3C%3D%3E%20bar'
+ * encode('foo "#&\'<=> bar')
+ *
+ * // returns ''
+ * encode(['foobar'])
+ *
+ * @param {String} value String to percent-encode
+ * @returns {String} Percent-encoded string
+ */
+function encode (value) {
+    return encoder.percentEncode(value, QUERY_ENCODE_SET);
+}
+
+/**
+ * Percent-encode the URL query string or x-www-form-urlencoded body object
+ * according to RFC3986.
+ *
+ * @example
+ * // returns 'q1=foo&q2=bar&q2=baz'
+ * encodeQueryParams({ q1: 'foo', q2: ['bar', 'baz'] })
+ *
+ * @param {Object} query Object representing query or urlencoded body
+ * @returns {String} Percent-encoded string
+ */
+function encodeQueryString (query) {
+    if (!(query && typeof query === OBJECT)) {
+        return E;
+    }
+
+    // rely upon faster querystring module
+    query = querystring.stringify(query);
+
+    // encode characters not encoded by querystring.stringify() according to RFC3986.
+    return query.replace(/[!'()*]/g, function (c) {
+        return encoder.percentEncodeCharCode(c.charCodeAt(0));
+    });
+}
+
+/**
+ * Converts PostmanUrl / URL string into Node.js compatible Url object.
+ *
+ * @example <caption>Using URL string</caption>
+ * toNodeUrl('郵便屋さん.com/foo&bar/{baz}?q=("foo")#`hash`')
+ * // returns
+ * // {
+ * //     protocol: 'http:',
+ * //     slashes: true,
+ * //     auth: null,
+ * //     host: 'xn--48jwgn17gdel797d.com',
+ * //     port: null,
+ * //     hostname: 'xn--48jwgn17gdel797d.com',
+ * //     hash: '#%60hash%60',
+ * //     search: '?q=(%22foo%22)',
+ * //     query: 'q=(%22foo%22)',
+ * //     pathname: '/foo&bar/%7Bbaz%7D',
+ * //     path: '/foo&bar/%7Bbaz%7D?q=(%22foo%22)',
+ * //     href: 'http://xn--48jwgn17gdel797d.com/foo&bar/%7Bbaz%7D?q=(%22foo%22)#%60hash%60'
+ * //  }
+ *
+ * @example <caption>Using PostmanUrl instance</caption>
+ * toNodeUrl(new sdk.Url({
+ *     host: 'example.com',
+ *     query: [{ key: 'foo', value: 'bar & baz' }]
+ * }))
+ *
+ * @param {PostmanUrl|String} url
+ * @returns {Url}
+ */
+function toNodeUrl (url) {
+    var nodeUrl = {
+        protocol: null,
+        slashes: null,
+        auth: null,
+        host: null,
+        port: null,
+        hostname: null,
+        hash: null,
+        search: null,
+        query: null,
+        pathname: null,
+        path: null,
+        href: E
+    };
+
+    // convert URL string to PostmanUrl
+    if (typeof url === STRING) {
+        url = new sdk.Url(url);
+    }
+
+    // bail out if given url is not a PostmanUrl instance
+    if (!sdk.Url.isUrl(url)) {
+        return nodeUrl;
+    }
+
+    // #protocol
+    nodeUrl.protocol = (typeof url.protocol === STRING) ?
+        url.protocol.replace(PROTOCOL_SEPARATOR, E).toLowerCase() :
+        DEFAULT_PROTOCOL;
+    nodeUrl.protocol += COLON;
+
+    // #slashes
+    nodeUrl.slashes = SLASHED_PROTOCOLS.has(nodeUrl.protocol);
+
+    // #href = protocol://
+    nodeUrl.href = nodeUrl.protocol + DOUBLE_SLASH;
+
+    // #auth
+    if (url.auth) {
+        if (typeof url.auth.user === STRING) {
+            nodeUrl.auth = encoder.encodeUserInfo(url.auth.user);
+        }
+        if (typeof url.auth.password === STRING) {
+            !nodeUrl.auth && (nodeUrl.auth = E);
+            nodeUrl.auth += COLON + encoder.encodeUserInfo(url.auth.password);
+        }
+
+        // #href = protocol://user:password@
+        nodeUrl.auth && (nodeUrl.href += nodeUrl.auth + AUTH_CREDENTIALS_SEPARATOR);
+    }
+
+    // #host, #hostname
+    nodeUrl.host = nodeUrl.hostname = encoder.encodeHost(url.getHost()).toLowerCase();
+
+    // #href = protocol://user:password@host.name
+    nodeUrl.href += nodeUrl.hostname;
+
+    // @todo Add helper in SDK to normalize port
+    if (typeof (url.port && url.port.toString) === FUNCTION) {
+        // #port
+        nodeUrl.port = url.port.toString();
+
+        // #host = (#hostname):(#port)
+        nodeUrl.host = (nodeUrl.hostname || E) + COLON + nodeUrl.port;
+
+        // #href = protocol://user:password@host.name:port
+        nodeUrl.href += COLON + nodeUrl.port;
+    }
+
+    // #path, #pathname
+    nodeUrl.path = nodeUrl.pathname = encoder.encodePath(url.getPath());
+
+    // #href = protocol://user:password@host.name:port/p/a/t/h
+    nodeUrl.href += nodeUrl.pathname;
+
+    if (url.query.count()) {
+        // #query
+        nodeUrl.query = encoder.encodeQueryParams(url.query.all());
+
+        // #search
+        nodeUrl.search = QUERY_SEPARATOR + nodeUrl.query;
+
+        // #path = (#pathname)?(#search)
+        nodeUrl.path = nodeUrl.pathname + nodeUrl.search;
+
+        // #href = protocol://user:password@host.name:port/p/a/t/h?q=query
+        nodeUrl.href = (nodeUrl.href || E) + nodeUrl.search;
+    }
+
+    if (url.hash) {
+        // #hash
+        nodeUrl.hash = SEARCH_SEPARATOR + encoder.encodeFragment(url.hash);
+
+        // #href = protocol://user:password@host.name:port/p/a/t/h?q=query#hash
+        nodeUrl.href += nodeUrl.hash;
+    }
+
+    return nodeUrl;
+}
+
+/**
+ * Converts URL string into Node.js compatible Url object using the v1 encoder.
+ *
+ * @deprecated since version 2.0
+ *
+ * @param {String} url URL string
+ * @returns {Url} Node.js compatible Url object
+ */
+function toLegacyNodeUrl (url) {
+    return legacy.toNodeUrl(url);
+}
+
+module.exports = {
+    encode,
+    toNodeUrl,
+    toLegacyNodeUrl,
+    encodeQueryString
+};
