28838472a2494629bd9f036e9938c0c455d1b0c1
Udit Vasu <udit.vasu@getpostman.com>
2019-12-29 00:31:07 +0530

    Add unit tests for .toNodeUrl properties

--- test/unit/toNodeUrl.test.js
+++ test/unit/toNodeUrl.test.js
@@ -75,6 +75,257 @@ describe('.toNodeUrl', function () {
         expect(toNodeUrl({ host: 'example.com' })).to.eql(defaultUrl);
     });
 
+    describe('PROPERTY', function () {
+        describe('.protocol', function () {
+            it('should defaults to http:', function () {
+                expect(toNodeUrl('example.com')).to.have.property('protocol', 'http:');
+            });
+
+            it('should defaults to http: for non-string protocols', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    protocol: { protocol: 'https' }
+                }))).to.have.property('protocol', 'http:');
+            });
+
+            it('should handle custom protocols', function () {
+                expect(toNodeUrl('postman://example.com')).to.have.property('protocol', 'postman:');
+            });
+        });
+
+        describe('.slashes', function () {
+            it('should be true for file:, ftp:, gopher:, http:, and ws: protocols', function () {
+                expect(toNodeUrl('file://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('ftp://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('gopher://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('http://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('https://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('ws://example.com')).to.have.property('slashes', true);
+                expect(toNodeUrl('wss://example.com')).to.have.property('slashes', true);
+            });
+
+            it('should be false for custom protocols', function () {
+                expect(toNodeUrl('postman://example.com')).to.have.property('slashes', false);
+            });
+        });
+
+        describe('.auth', function () {
+            it('should percent-encode the reserved and unicode characters', function () {
+                expect(toNodeUrl('`user`:pâ$$@example.com'))
+                    .to.have.property('auth', '%60user%60:p%C3%A2$$');
+            });
+
+            it('should not double encode the characters', function () {
+                expect(toNodeUrl('%22user%22:p%C3%A2$$@example.com'))
+                    .to.have.property('auth', '%22user%22:p%C3%A2$$');
+            });
+
+            it('should ignore the empty and non-string username', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    auth: {
+                        user: ['root'],
+                        password: 'secret'
+                    }
+                }))).to.have.property('auth', ':secret');
+
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    auth: {
+                        password: 'secret#123'
+                    }
+                }))).to.have.property('auth', ':secret%23123');
+            });
+
+            it('should ignore the empty and non-string password', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    auth: {
+                        user: 'root',
+                        password: 12345
+                    }
+                }))).to.have.property('auth', 'root');
+
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    auth: {
+                        user: 'root@domain.com'
+                    }
+                }))).to.have.property('auth', 'root%40domain.com');
+            });
+        });
+
+        describe('.host and .hostname', function () {
+            it('should do punycode ASCII serialization of the domain', function () {
+                expect(toNodeUrl('😎.cool')).to.include({
+                    host: 'xn--s28h.cool',
+                    hostname: 'xn--s28h.cool'
+                });
+
+                expect(toNodeUrl('postman.com')).to.include({
+                    host: 'postman.com',
+                    hostname: 'postman.com'
+                });
+
+                expect(toNodeUrl('郵便屋さん.com')).to.include({
+                    host: 'xn--48jwgn17gdel797d.com',
+                    hostname: 'xn--48jwgn17gdel797d.com'
+                });
+            });
+
+            it('should handle the IP address shorthands', function () {
+                expect(toNodeUrl('0')).to.include({
+                    host: '0.0.0.0',
+                    hostname: '0.0.0.0'
+                });
+
+                expect(toNodeUrl('1234')).to.include({
+                    host: '0.0.4.210',
+                    hostname: '0.0.4.210'
+                });
+
+                expect(toNodeUrl('127.1')).to.include({
+                    host: '127.0.0.1',
+                    hostname: '127.0.0.1'
+                });
+
+                expect(toNodeUrl('255.255.255')).to.include({
+                    host: '255.255.0.255',
+                    hostname: '255.255.0.255'
+                });
+
+            });
+
+            it('should not double encode hostname', function () {
+                expect(toNodeUrl('xn--48jwgn17gdel797d.com')).to.include({
+                    host: 'xn--48jwgn17gdel797d.com',
+                    hostname: 'xn--48jwgn17gdel797d.com'
+                });
+            });
+
+            it('should handle invalid hostname', function () {
+                expect(toNodeUrl('xn:')).to.include({
+                    host: 'xn:',
+                    hostname: 'xn:'
+                });
+
+                expect(toNodeUrl('xn--iñvalid.com')).to.include({
+                    host: 'xn--iñvalid.com',
+                    hostname: 'xn--iñvalid.com'
+                });
+            });
+
+            it('should add port to the host but not to the hostname', function () {
+                expect(toNodeUrl('example.com:399')).to.include({
+                    host: 'example.com:399',
+                    hostname: 'example.com'
+                });
+            });
+        });
+
+        describe('.port', function () {
+            it('should accept port as string', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    port: '399'
+                }))).to.have.property('port', '399');
+            });
+
+            it('should accept port as number', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    port: 399
+                }))).to.have.property('port', '399');
+            });
+        });
+
+        describe('.hash', function () {
+            it('should percent-encode the reserved and unicode characters', function () {
+                expect(toNodeUrl('example.com#(😎)')).to.have.property('hash', '#(%F0%9F%98%8E)');
+            });
+
+            it('should not double encode the characters', function () {
+                expect(toNodeUrl('example.com#(%F0%9F%98%8E)')).to.have.property('hash', '#(%F0%9F%98%8E)');
+            });
+
+            it('should percent-encode SPACE, ("), (<), (>), and (`)', function () {
+                expect(toNodeUrl('0# "<>`')).to.have.property('hash', '#%20%22%3C%3E%60');
+            });
+        });
+
+        describe('.query and .search', function () {
+            it('should percent-encode the reserved and unicode characters', function () {
+                expect(toNodeUrl('example.com?q1=(1 2)&q2=𝌆й你ス')).to.include({
+                    query: 'q1=(1%202)&q2=%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9',
+                    search: '?q1=(1%202)&q2=%F0%9D%8C%86%D0%B9%E4%BD%A0%E3%82%B9'
+                });
+            });
+
+            it('should not double encode the characters', function () {
+                expect(toNodeUrl('example.com?q1=(1%202)&q2=f%C3%B2%C3%B3')).to.include({
+                    query: 'q1=(1%202)&q2=f%C3%B2%C3%B3',
+                    search: '?q1=(1%202)&q2=f%C3%B2%C3%B3'
+                });
+            });
+
+            it('should percent-encode SPACE, ("), (#), (&), (\'), (<), (=), and (>)', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    query: [' ', '"', '#', '&', '\'', '<', '=', '>']
+                }))).to.include({
+                    query: '%20=&%22=&%23=&%26=&%27=&%3C=&%3E=',
+                    search: '?%20=&%22=&%23=&%26=&%27=&%3C=&%3E='
+                });
+            });
+        });
+
+        describe('.path and pathname', function () {
+            it('should percent-encode the reserved and unicode characters', function () {
+                expect(toNodeUrl('example.com/foo/你ス/(⚡️)')).to.include({
+                    path: '/foo/%E4%BD%A0%E3%82%B9/(%E2%9A%A1%EF%B8%8F)',
+                    pathname: '/foo/%E4%BD%A0%E3%82%B9/(%E2%9A%A1%EF%B8%8F)'
+                });
+            });
+
+            it('should not double encode the characters', function () {
+                expect(toNodeUrl('example.com/foo/%E4%BD%A0%E3%82%B9/(bar)/')).to.include({
+                    path: '/foo/%E4%BD%A0%E3%82%B9/(bar)/',
+                    pathname: '/foo/%E4%BD%A0%E3%82%B9/(bar)/'
+                });
+            });
+
+            it('should percent-encode SPACE, ("), (<), (>), (`), (#), (?), ({), and (})', function () {
+                expect(toNodeUrl(new PostmanUrl({
+                    host: 'example.com',
+                    path: [' ', '"', '<', '>', '`', '#', '?', '{', '}']
+                }))).to.include({
+                    path: '/%20/%22/%3C/%3E/%60/%23/%3F/%7B/%7D',
+                    pathname: '/%20/%22/%3C/%3E/%60/%23/%3F/%7B/%7D'
+                });
+            });
+
+            it('should add query to the path but not to the pathname', function () {
+                expect(toNodeUrl('example.com/foo?q=bar')).to.include({
+                    path: '/foo?q=bar',
+                    pathname: '/foo'
+                });
+            });
+        });
+
+        describe('.href', function () {
+            it('should percent-encode the reserved and unicode characters', function () {
+                expect(toNodeUrl('ròót@郵便屋さん.com/[⚡️]?q1=(1 2)#%foo%')).to.include({
+                    href: 'http://r%C3%B2%C3%B3t@xn--48jwgn17gdel797d.com/[%E2%9A%A1%EF%B8%8F]?q1=(1%202)#%foo%'
+                });
+            });
+
+            it('should not double encode the characters', function () {
+                expect(toNodeUrl('postman://xn--48jwgn17gdel797d.com/[%E2%9A%A1%EF%B8%8F]?q1=(1%202)')).to.include({
+                    href: 'postman://xn--48jwgn17gdel797d.com/[%E2%9A%A1%EF%B8%8F]?q1=(1%202)'
+                });
+            });
+        });
+    });
+
     describe('FIXTURES', function () {
         describe('with url string', function () {
             var list = require('../fixtures/string-url-to-node-url');
