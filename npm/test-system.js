142f7a149dfd95aff8554364e430f7b2e9447df0
Udit Vasu <udit@pm.me>
2020-10-04 22:21:11 +0530

    Add system test script

---
+++ npm/test-system.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+// ---------------------------------------------------------------------------------------------------------------------
+// This script is intended to execute all system tests.
+// ---------------------------------------------------------------------------------------------------------------------
+
+const path = require('path'),
+
+    Mocha = require('mocha'),
+    chalk = require('chalk'),
+    recursive = require('recursive-readdir'),
+
+    SPEC_SOURCE_DIR = path.join(__dirname, '..', 'test', 'system');
+
+module.exports = function (exit) {
+    // banner line
+    console.info(chalk.yellow.bold('\nRunning system tests using mocha...'));
+
+    // add all spec files to mocha
+    recursive(SPEC_SOURCE_DIR, (err, files) => {
+        if (err) {
+            console.error(err);
+
+            return exit(1);
+        }
+
+        const mocha = new Mocha({ timeout: 1000 * 60 });
+
+        files.filter((file) => { // extract all test files
+            return (file.substr(-8) === '.test.js');
+        }).forEach(mocha.addFile.bind(mocha));
+
+        // start the mocha run
+        mocha.run((runError) => {
+            if (runError) {
+                console.error(runError.stack || runError);
+
+                return exit(1);
+            }
+
+            exit();
+        });
+    });
+};
+
+// ensure we run this script exports if this is a direct stdin.tty run
+!module.parent && module.exports(process.exit);
